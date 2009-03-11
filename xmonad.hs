3ff49e157435bd3fefc64d8e327db5fa59ff6467
Matt Burrows <maburrow@googlemail.com>
2009-03-11 19:32:59 +0000

    Added custom mouse bindings.
    The coolest edition was to map the tilt left and right buttons to next
    and prev WS. Very useful with my new Logitech VX Nano mouse :-)

--- xmonad.hs
+++ xmonad.hs
@@ -21,6 +21,7 @@ main = do
                          ppOutput = hPutStrLn xmproc,
                          ppTitle = xmobarColor "green" "" . shorten 50
                        },
+         mouseBindings = myMouseBindings,
          keys        =  \c -> myKeys c `M.union` keys defaultConfig c
          } where 
              myKeys conf@(XConfig {XMonad.modMask = modMask, workspaces = ws}) = M.fromList $
@@ -49,3 +50,28 @@ myLayout = tabbed shrinkText myTabConfig ||| tiled ||| Mirror tiled ||| Full
      delta   = 3/100
  
 myTabConfig = defaultTheme { decoHeight = 14 }
+
+button6     =  6 :: Button
+button7     =  7 :: Button
+
+myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
+    -- mod-button1, Set the window to floating mode and move by dragging
+    [ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))
+    -- mod-button2, Raise the window to the top of the stack
+    , ((modMask, button2), (\w -> focus w >> windows W.swapMaster))
+    -- mod-button3, Set the window to floating mode and resize by dragging
+    , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w))
+    -- you may also bind events to the mouse scroll wheel (button4 and button5)
+    -- cycle focus
+    , ((modMask, button4), (\_ -> windows W.focusUp))
+    , ((modMask, button5), (\_ -> windows W.focusDown))
+    -- cycle through workspaces
+    , ((0, button6), prevNonEmptyWS)
+    , ((0, button7), nextNonEmptyWS)
+    ]
+    where 
+      nextNonEmptyWS = \_ -> moveTo Next NonEmptyWS
+      prevNonEmptyWS = \_ -> moveTo Prev NonEmptyWS
+
+
+
