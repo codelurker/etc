1af0c070021343460a28939495bfeb38596b2b78
Matt Burrows <mburrows@mburrows-lt.(none)>
2009-12-21 22:40:41 +0000

    Changed xmobar font to Inconsolata and made trayer opaque

--- xmonad.sh
+++ xmonad.sh
@@ -3,7 +3,7 @@
 xrdb -merge ~/.Xdefaults
 xmodmap ~/.xmodmap
 
-trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --height 12 --transparent true --tint 0x000000 &
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --height 12 --transparent true --tint 0x000000 --alpha 0 &
 gnome-settings-daemon &
 gnome-power-manager &
 gnome-screensaver &
